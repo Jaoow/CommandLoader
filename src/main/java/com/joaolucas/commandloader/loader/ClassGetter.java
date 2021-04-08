73fc8e09860ec72bef7ff240878aafb7ffff83fd
joao-py <xfx.joao@gmail.com>
2021-04-07 23:54:30 -0300

    Code improvement

--- src/main/java/com/joaolucas/commandloader/loader/ClassGetter.java
+++ src/main/java/com/joaolucas/commandloader/loader/ClassGetter.java
@@ -1,5 +1,7 @@
 package com.joaolucas.commandloader.loader;
 
+import com.google.common.collect.Lists;
+import com.google.common.reflect.ClassPath;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -7,65 +9,25 @@ import java.net.URL;
 import java.security.CodeSource;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 public class ClassGetter {
 
-    public static ArrayList<Class<?>> getClassesForPackage(JavaPlugin plugin, String packageName) {
-        ArrayList<Class<?>> classes = new ArrayList<>();
-        CodeSource src = plugin.getClass().getProtectionDomain().getCodeSource();
-        if (src != null) {
-            URL resource = src.getLocation();
-            resource.getPath();
-            processJar(resource, packageName, classes);
-        }
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<Class<?>> clazz = new ArrayList<>();
-        for (Class<?> classy : classes) {
-            names.add(classy.getSimpleName());
-            clazz.add(classy);
-        }
-        classes.clear();
-        names.sort(String.CASE_INSENSITIVE_ORDER);
-        for (String s : names) {
-            for (Class<?> classy2 : clazz) {
-                if (classy2.getSimpleName().equals(s)) {
-                    classes.add(classy2);
-                    break;
-                }
-            }
-        }
-        return classes;
-    }
-
-    private static Class<?> loadClass(String className) {
+    @SuppressWarnings("UnstableApiUsage")
+    public static List<Class<?>> getClassesFromPackage(JavaPlugin plugin, String packageName) {
+        ClassLoader classLoader = plugin.getClass().getClassLoader();
         try {
-            return Class.forName(className);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Unexpected ClassNotFoundException loading class '" + className + "'");
+            return ClassPath.from(classLoader).getTopLevelClasses()
+                    .stream()
+                    .filter(classInfo -> classInfo.getName().toLowerCase().startsWith(packageName))
+                    .map(ClassPath.ClassInfo::load)
+                    .collect(Collectors.toList());
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
-    }
-
-    private static void processJar(URL resource, String pkgname, ArrayList<Class<?>> classes) {
-        JarFile jarFile;
-        String relPath = pkgname.replace('.', '/');
-        String resPath = resource.getPath().replace("%20", " ");
-        String jarPath = resPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
-        try {
-            jarFile = new JarFile(jarPath);
-        } catch (IOException e) {
-            throw new RuntimeException("Unexpected IOException reading JAR File '" + jarPath + "'", e);
-        }
-        Enumeration<JarEntry> entries = jarFile.entries();
-        while (entries.hasMoreElements()) {
-            JarEntry entry = entries.nextElement();
-            String entryName = entry.getName();
-            String className = null;
-            if (entryName.endsWith(".class") && entryName.startsWith(relPath) && entryName.length() > relPath.length() + "/".length())
-                className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
-            if (className != null)
-                classes.add(loadClass(className));
-        }
+        return Lists.newArrayList();
     }
 }
--- src/main/java/com/joaolucas/commandloader/loader/Loader.java
+++ src/main/java/com/joaolucas/commandloader/loader/Loader.java
@@ -7,14 +7,7 @@ import org.bukkit.command.CommandMap;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
-import java.net.URL;
-import java.security.CodeSource;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 
 /**
@@ -51,7 +44,7 @@ public class Loader {
     public void load(Type type, String packageName) {
         switch (type) {
             case COMMANDS:
-                ClassGetter.getClassesForPackage(mainLoader, packageName).forEach(classes -> {
+                ClassGetter.getClassesFromPackage(mainLoader, packageName).forEach(classes -> {
                     try {
                         if (Command.class.isAssignableFrom(classes)) {
                             Command cmd = (Command) classes.newInstance();
@@ -64,7 +57,7 @@ public class Loader {
                 });
                 break;
             case LISTENER:
-                ClassGetter.getClassesForPackage(mainLoader, packageName).forEach(classes -> {
+                ClassGetter.getClassesFromPackage(mainLoader, packageName).forEach(classes -> {
                     try {
                         if (Listener.class.isAssignableFrom(classes)) {
                             Listener listener = (Listener) classes.newInstance();
@@ -81,65 +74,5 @@ public class Loader {
 
     public enum Type {
         LISTENER, COMMANDS;
-    }
-
-
-    public static class ClassGetter {
-
-        public static ArrayList<Class<?>> getClassesForPackage(JavaPlugin plugin, String packageName) {
-            ArrayList<Class<?>> classes = new ArrayList<>();
-            CodeSource src = plugin.getClass().getProtectionDomain().getCodeSource();
-            if (src != null) {
-                URL resource = src.getLocation();
-                processJar(resource, packageName, classes);
-            }
-            ArrayList<String> names = new ArrayList<>();
-            ArrayList<Class<?>> clazz = new ArrayList<>();
-            for (Class<?> classy : classes) {
-                names.add(classy.getSimpleName());
-                clazz.add(classy);
-            }
-            classes.clear();
-            names.sort(String.CASE_INSENSITIVE_ORDER);
-            for (String s : names) {
-                for (Class<?> classy2 : clazz) {
-                    if (classy2.getSimpleName().equals(s)) {
-                        classes.add(classy2);
-                        break;
-                    }
-                }
-            }
-            return classes;
-        }
-
-        private static Class<?> loadClass(String className) {
-            try {
-                return Class.forName(className);
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException("Unexpected ClassNotFoundException loading class '" + className + "'");
-            }
-        }
-
-        private static void processJar(URL resource, String packageName, ArrayList<Class<?>> classes) {
-            JarFile jarFile;
-            String relPath = packageName.replace('.', '/');
-            String resPath = resource.getPath().replace("%20", " ");
-            String jarPath = resPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
-            try {
-                jarFile = new JarFile(jarPath);
-            } catch (IOException e) {
-                throw new RuntimeException("Unexpected IOException reading JAR File '" + jarPath + "'", e);
-            }
-            Enumeration<JarEntry> entries = jarFile.entries();
-            while (entries.hasMoreElements()) {
-                JarEntry entry = entries.nextElement();
-                String entryName = entry.getName();
-                String className = null;
-                if (entryName.endsWith(".class") && entryName.startsWith(relPath) && entryName.length() > relPath.length() + "/".length())
-                    className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
-                if (className != null)
-                    classes.add(loadClass(className));
-            }
-        }
     }
 }
